bfc0799f7b805e01725e49e4eaceeaf7e6a45cfe
lloubiere <lloubiere@lloubiere-PC>
2018-03-05 13:25:26 -0300

    revision 28.02

--- bs_produccion/src/main/java/bs/produccion/web/ComprobanteProduccionBean.java
+++ bs_produccion/src/main/java/bs/produccion/web/ComprobanteProduccionBean.java
@@ -1,166 +1,233 @@
 package bs.produccion.web;
 
-///*
-// * To change this template, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//
-//package bs.produccion.web;
-//
-//import isd.general.util.ABMEntidades;
-//
-//import isd.general.web.GenericBean;
-//import isd.inventario.rn.SectorRN;
-//import bs.produccion.modelo.ComprobanteProduccion;
-//import bs.produccion.modelo.Operario;
-//import bs.produccion.rn.PDComprobantesRN;
-//import bs.produccion.rn.PDOperarioRN;
-//import java.util.HashMap;
-//import java.util.List;
-//import javax.annotation.PostConstruct;
-//import javax.ejb.EJB;
-//import javax.faces.bean.ManagedBean;
-//import javax.faces.bean.ViewScoped;
-//
-///**
-// *
-// * @author Claudio
-// */
-//@ManagedBean
-//@ViewScoped
-//public class ComprobanteProduccionBean extends GenericBean implements ABMEntidades<ComprobanteProduccion> {
-//
-//    @EJB private PDComprobantesRN comprobanteRN;
-//    @EJB protected SectorRN sectorRN;
-//
-//    private ComprobanteProduccion comprobante;
-//    private List<ComprobanteProduccion> lista;
-//
-//
-//    /** Creates a new instance of PD_operarioBean */
-//    public ComprobanteProduccionBean() {
-//    }
-//
-//    @PostConstruct
-//    public void init(){
-//
-//        try {
-//
-//           filtro = new HashMap<String, String>();
-//           filtro.put("auditoria.debaja =", "'N'");
-//           lista = comprobanteRN.getLista(filtro);
-//           titulo = "Comprobantes de Producción";
-//           nuevo();
-//           
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            JsfUtil.addErrorMessage("Error al iniciar el bean " + e.getMessage());
-//        }
-//    }
-//
-//    @Override
-//    public void nuevo() {
-//        comprobante = new ComprobanteProduccion();
-//        nuevo = true;
-//    }
-//
-//    @Override
-//    public void guardar(boolean nuevo) {
-//        try {
-//            comprobanteRN.guardar(comprobante);
-//            lista = comprobanteRN.getLista(filtro);
-//            nuevo = false;
-//            JsfUtil.addInfoMessage("Los datos han sido guardados correctamente");
-//
-//            if(nuevo){
-//                nuevo();
-//            }
-//        } catch (Exception ex) {
-//            JsfUtil.addErrorMessage("No es posible guardar los datos");
-//        }
-//    }
-//   
-//    @Override
-//    public void eliminar(ComprobanteProduccion o) {
-//
-//        JsfUtil.addErrorMessage("No es posible borrar los datos");
-////
-////
-////        try {
-////            comprobanteRN.eliminar(o);
-////            lista = comprobanteRN.getLista(filtro);
-////            nuevo();
-////            JsfUtil.addWarningMessage("Los datos han sido borrados");
-////        } catch (Exception ex) {
-////            JsfUtil.addErrorMessage("No es posible borrar los datos");
-////        }
-//        
-//    }
-//
-//    @Override
-//    public void deBaja() {
-//        comprobante.getAuditoria().setDebaja("S");
-//        guardar(false);
-//        nuevo = false;
-//    }
-//
-//    @Override
-//    public void seleccionar(ComprobanteProduccion entidad) {
-//
-//        comprobante = entidad;
-//        nuevo = false;
-//        
-//    }
-//
-//    @Override
-//    public void imprimirLista(){
-//
-//    }
-//
-//    @Override
-//    public void imprimirEntidad(){
-//        
-//    }
-//
-//    public void limipiarSector(String tipoDeposito){
-//
-//        //Por ahora dejamos siempre los sectores cero por defecto
-//        try {
-//            if (tipoDeposito.equals("I") && comprobante.getDepositoIngreso() != null) {
-//
-//                comprobante.setSectorIngreso(sectorRN.getSector(comprobante.getDepositoIngreso().getDeposi(), "0"));
-//            }
-//
-//            if (tipoDeposito.equals("E") && comprobante.getDepositoEgreso() != null) {
-//
-//                comprobante.setSectorEgreso(sectorRN.getSector(comprobante.getDepositoEgreso().getDeposi(), "0"));
-//            }
-//        } catch (Exception e) {
-//
-//            JsfUtil.addErrorMessage("No es posible limpiar el sector del deposito");
-//        }
-//    }
-//
-//    public ComprobanteProduccion getComprobante() {
-//        return comprobante;
-//    }
-//
-//    public void setComprobante(ComprobanteProduccion comprobante) {
-//        this.comprobante = comprobante;
-//    }
-//
-//    public List<ComprobanteProduccion> getLista() {
-//        return lista;
-//    }
-//
-//    public void setLista(List<ComprobanteProduccion> lista) {
-//        this.lista = lista;
-//    }
-//
-//    @Override
-//    public String verEntidad(ComprobanteProduccion entidad) {
-//        throw new UnsupportedOperationException("Not supported yet.");
-//    }
-//
-//    
-//}
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the e
+ */
+
+import bs.global.modelo.FormularioPorSituacionIVA;
+import bs.global.rn.FormularioPorSituacionIVARN;
+import bs.global.util.JsfUtil;
+import bs.global.web.FormularioSituacionIVABean;
+import bs.global.web.GenericBean;
+import bs.produccion.modelo.ComprobanteProduccion;
+import bs.produccion.rn.ComprobanteProduccionRN;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author Claudio
+ */
+@ManagedBean
+@ViewScoped
+public class ComprobanteProduccionBean extends GenericBean implements Serializable{
+
+    @EJB private ComprobanteProduccionRN comprobanteRN;
+    @EJB private FormularioPorSituacionIVARN formularioSituacionIvaRN;
+        
+    private ComprobanteProduccion comprobante;
+    private List<ComprobanteProduccion> lista;
+    
+    private boolean mostrarConceptoDebaja;
+    private boolean mostrarFormularioSituacionIvaDebaja;
+    
+    @ManagedProperty(value = "#{formularioSituacionIVABean}")
+    private FormularioSituacionIVABean formularioSituacionIVABean;
+    
+    /**
+     * Creates a new instance of ComprobanteProduccionBean
+     */
+    public ComprobanteProduccionBean() {
+        
+    }
+    
+    @PostConstruct
+    public void init(){
+        
+        super.iniciar();
+        
+        mostrarConceptoDebaja = false;        
+        nuevo();
+        buscar();        
+        formularioSituacionIVABean.iniciarFiltro("PD");
+    }
+    
+    public void nuevo(){
+        
+        esNuevo = true;
+        buscaMovimiento = false;
+        comprobante = new ComprobanteProduccion();
+        comprobante.setModulo("PD");
+    }
+    
+    public void guardar(boolean nuevo){
+                
+        try {
+            if (comprobante != null) {
+                
+                comprobanteRN.guardar(comprobante, esNuevo);
+                esNuevo = false;                
+                JsfUtil.addInfoMessage("Los datos se guardaron correctamente");
+                
+                if(nuevo){
+                    nuevo();
+                }                
+            }else{
+                JsfUtil.addInfoMessage("No hay datos para guardar");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(getClass().getSimpleName()).log(Level.SEVERE, null, ex);
+            JsfUtil.addErrorMessage("No es posible guardar los datos " + ex);
+        }
+    }
+    
+    public void habilitaDeshabilita(String habDes){
+        
+        try {
+            comprobante.getAuditoria().setDebaja(habDes);
+            comprobanteRN.guardar(comprobante, false);            
+            JsfUtil.addInfoMessage("Los datos se actualizaron correctamente");
+            
+        } catch (Exception ex) {
+            Logger.getLogger(getClass().getSimpleName()).log(Level.SEVERE, null, ex);
+            JsfUtil.addErrorMessage("No es posible borrar los datos " +  ex);
+        }
+    }
+        
+    public void eliminar(){
+        try {
+            comprobanteRN.eliminar(comprobante);
+            nuevo();            
+            JsfUtil.addInfoMessage("Los datos fueron borrados");
+        } catch (Exception ex) {
+            Logger.getLogger(getClass().getSimpleName()).log(Level.SEVERE, null, ex);
+            JsfUtil.addErrorMessage("No es posible borrar los datos " +  ex);
+        }        
+    }
+    
+    public void buscar(){
+        lista = comprobanteRN.getListaByBusqueda(txtBusqueda, mostrarDebaja, cantidadRegistros);        
+    } 
+    
+    public List<ComprobanteProduccion> complete(String query) {
+        try {
+            lista = comprobanteRN.getListaByBusqueda(query, false,cantidadRegistros);    
+            return lista;
+            
+        } catch (Exception ex) {
+
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
+            return new ArrayList<ComprobanteProduccion>();
+        }
+    }
+    
+    public void onSelect(SelectEvent event) {        
+        comprobante = (ComprobanteProduccion) event.getObject(); 
+        esNuevo = false;
+        buscaMovimiento = false;
+    }
+    
+    public void seleccionar(ComprobanteProduccion v){
+        
+        comprobante = v;
+        esNuevo = false;
+        buscaMovimiento = false;
+    }
+
+    public void imprimir(){
+        
+        if(comprobante==null){
+            JsfUtil.addErrorMessage("No hay entidad seleccionada, nada para imprimir");
+        }        
+    }
+        
+    public void nuevoFormularioSituacionIva(){
+        
+        if(comprobante!=null){
+            
+            if(comprobante.getFormulariosPorSituacionIVA()==null){
+                comprobante.setFormulariosPorSituacionIVA(new ArrayList<FormularioPorSituacionIVA>());
+            }
+            
+            formularioSituacionIVABean.setComprobante(comprobante);
+            formularioSituacionIVABean.nuevo();
+            
+        }else{
+            
+            JsfUtil.addErrorMessage("No existe un comprobante para asociarle un formulario por situación de iva");
+        }        
+    }
+    
+    public void seleccionarFormulario(FormularioPorSituacionIVA fsi){
+        
+        if(comprobante!=null){
+            
+            formularioSituacionIVABean.seleccionar(fsi);
+            
+        }else{
+            
+            JsfUtil.addErrorMessage("No existe un comprobante para asociarle un formulario por situación de iva");
+        }           
+    }
+    
+    public void sincronizarFormulario(){
+        
+        if(comprobante!=null){
+            comprobante.setFormulariosPorSituacionIVA(formularioSituacionIvaRN.getListaByComprobante(comprobante, mostrarFormularioSituacionIvaDebaja));            
+        }
+    }
+    
+    //---------------------------------------------------------------------------
+    
+    public ComprobanteProduccion getComprobante() {
+        return comprobante;
+    }
+
+    public void setComprobante(ComprobanteProduccion comprobante) {
+        this.comprobante = comprobante;
+    }
+
+    public List<ComprobanteProduccion> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<ComprobanteProduccion> lista) {
+        this.lista = lista;
+    }
+
+    public boolean isMostrarConceptoDebaja() {
+        return mostrarConceptoDebaja;
+    }
+
+    public void setMostrarConceptoDebaja(boolean mostrarConceptoDebaja) {
+        this.mostrarConceptoDebaja = mostrarConceptoDebaja;
+    }
+
+    public FormularioSituacionIVABean getFormularioSituacionIVABean() {
+        return formularioSituacionIVABean;
+    }
+
+    public void setFormularioSituacionIVABean(FormularioSituacionIVABean formularioSituacionIVABean) {
+        this.formularioSituacionIVABean = formularioSituacionIVABean;
+    }
+
+    public boolean isMostrarFormularioSituacionIvaDebaja() {
+        return mostrarFormularioSituacionIvaDebaja;
+    }
+
+    public void setMostrarFormularioSituacionIvaDebaja(boolean mostrarFormularioSituacionIvaDebaja) {
+        this.mostrarFormularioSituacionIvaDebaja = mostrarFormularioSituacionIvaDebaja;
+    }
+    
+}
